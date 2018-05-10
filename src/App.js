7e8ab66896d7d1ead2621ef40708877656a4854d
Bhavesh Kumar <bhaveshkumar19856@gmail.com>
2018-05-10 22:01:20 +0530

    timer added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to Timer App built in React js</h2>
+        </div>
+
+        <Timer start={Date.now()}/>
       </div>
     );
   }
